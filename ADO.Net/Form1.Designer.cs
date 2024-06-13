e2b7b9599b1671fd894f8e9357ada2c1296e32b8
Reza FerdosAra <Reza FerdosAra@REZA_PC>
2024-06-13 18:09:05 +0330

    Add project files.

---
+++ ADO.Net/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace ADO.Net
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnAdd = new Button();
+            SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(12, 12);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(121, 31);
+            btnAdd.TabIndex = 0;
+            btnAdd.Text = "Add Product";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(513, 318);
+            Controls.Add(btnAdd);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnAdd;
+    }
+}
---
+++ ADO.Net/Form1.cs
@@ -0,0 +1,42 @@
+using Microsoft.Data.SqlClient;
+
+namespace ADO.Net
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*
+         * ADO.Net Steps:
+         *  *. Before Write Code, Must Install : Microsoft.Data.SqlClient
+         *      1. Write Connection String
+         *      2. Create Instance Of SqlConnection
+         *      3. Create Instance Of SqlCommand
+         *      4. Write Query On SqlCommand.Text
+         *      5. SqlConnection Open
+         *      6. EXEC SqlCommand.Text
+         *      7. SqlConnection Close
+         *      
+         */
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            /// With Username & Password
+            string connectionString = "Server=.;Database=Northwind;User Id=sa;Password=828898;TrustServerCertificate=True;Integrated Security=true;";
+            /// With Windows Authnticate
+            string connectionString2 = "Server=.;Database=Northwind;Trusted_Connection=True;TrustServerCertificate=True;Integrated Security=true;";
+
+            var connection = new SqlConnection(connectionString2);
+            var command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandText = "INSERT INTO dbo.Products (ProductName, UnitPrice) Values ('Coca Cola Testi', 2.5)";
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+            MessageBox.Show("Successful Added");
+        }
+    }
+}
