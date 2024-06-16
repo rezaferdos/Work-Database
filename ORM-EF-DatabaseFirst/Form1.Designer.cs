9c431ceb3f53e6d05d704691fb97629b372bf0c3
RezaFerdosAra <ferdosarareza82fr@gmail.com>
2024-06-16 12:35:35 +0330

    Service written

--- ORM-EF-DatabaseFirst/Form1.Designer.cs
+++ ORM-EF-DatabaseFirst/Form1.Designer.cs
@@ -28,20 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dgProducts = new System.Windows.Forms.DataGridView();
+            this.btnLoadData = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgProducts)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dgProducts
+            // 
+            this.dgProducts.AllowUserToAddRows = false;
+            this.dgProducts.AllowUserToDeleteRows = false;
+            this.dgProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgProducts.Location = new System.Drawing.Point(12, 71);
+            this.dgProducts.Name = "dgProducts";
+            this.dgProducts.ReadOnly = true;
+            this.dgProducts.Size = new System.Drawing.Size(636, 326);
+            this.dgProducts.TabIndex = 0;
+            // 
+            // btnLoadData
+            // 
+            this.btnLoadData.Location = new System.Drawing.Point(12, 15);
+            this.btnLoadData.Name = "btnLoadData";
+            this.btnLoadData.Size = new System.Drawing.Size(82, 39);
+            this.btnLoadData.TabIndex = 1;
+            this.btnLoadData.Text = "Refresh Data";
+            this.btnLoadData.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(390, 15);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(82, 39);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "Update Data";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(478, 15);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(82, 39);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete Data";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(566, 15);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(82, 39);
+            this.btnInsert.TabIndex = 4;
+            this.btnInsert.Text = "Insert Data";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(302, 15);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(82, 39);
+            this.btnRead.TabIndex = 5;
+            this.btnRead.Text = "Read Data";
+            this.btnRead.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(570, 338);
+            this.ClientSize = new System.Drawing.Size(660, 409);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnLoadData);
+            this.Controls.Add(this.dgProducts);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgProducts)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgProducts;
+        private System.Windows.Forms.Button btnLoadData;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnRead;
     }
 }
 
---
+++ ORM-EF-DatabaseFirst/Service/Service.cs
@@ -0,0 +1,76 @@
+﻿using ORM_EF_DatabaseFirst.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_EF_DatabaseFirst.Service
+{
+    public class Service
+    {
+        /// ////////////////////////////////////////////////////////////
+        // Read All Data
+        public List<Products> GetAll()
+        {
+            using (var ctx = new NorthwindEntities())
+            {
+                return ctx.Products.ToList();
+            }
+        }
+        /// ////////////////////////////////////////////////////////////
+        // Get One Data
+        public Products GetByID(int Id)
+        {
+            using (var ctx = new NorthwindEntities())
+            {
+                var model = ctx.Products.FirstOrDefault(n => n.ProductID == Id);
+                return model;
+            }
+        }
+
+        /// ////////////////////////////////////////////////////////////
+        // Insert Data
+        public bool Insert(Products p)
+        {
+            using (var ctx = new NorthwindEntities())
+            {
+                ctx.Products.Add(p);
+                ctx.SaveChanges();
+                return true;
+            }
+        }
+
+        /// ////////////////////////////////////////////////////////////
+        // Remove Data
+        public bool Remove(Products p)
+        {
+            using (var ctx = new NorthwindEntities())
+            {
+                var model = ctx.Products.FirstOrDefault(n => n.ProductID == p.ProductID);
+                ctx.Products.Remove(model);
+                ctx.SaveChanges();
+                return true;
+            }
+        }
+
+        /// ////////////////////////////////////////////////////////////
+        // Update Data
+        public bool Update(Products p)
+        {
+            using (var ctx = new NorthwindEntities())
+            {
+                var model = ctx.Products.FirstOrDefault(n => n.ProductID == p.ProductID);
+                if (model != null)
+                {
+                    model.ProductName = p.ProductName;
+                    model.UnitPrice = p.UnitPrice;
+                    model.Discontinued = p.Discontinued;
+                }
+                ctx.SaveChanges();
+                return true;
+            }
+        }
+    }
+}
